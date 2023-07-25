c027cad4d622cefd771350d051f0c3d69d886b88
Mark Saville <mark.saville@outlook.com>
2023-07-25 19:57:04 +0200

    add json object, morgan and error handling

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+app.use(morgan('common'));
+
+app.use(express.static('public'));
+
+let topMovies = [
+  {
+    title: 'Fast and Furious 1', 
+  },
+  {
+    title: 'Fast and Furious 2',
+  },
+  {
+    title: 'Fast and Furious 3',
+  },
+  {
+    title: 'Fast and Furious 4',
+  },
+  {
+    title: 'Fast and Furious 5',
+  },
+  {
+    title: 'Fast and Furious 6',
+  },
+  {
+    title: 'Fast and Furious 7',
+  },
+  {
+    title: 'Fast and Furious 8',
+  },
+  {
+    title: 'Fast and Furious 9',
+  },
+  {
+    title: 'Fast and Furious 10',
+  }
+];
+
+// GET requests
+app.get('/', (req, res) => {
+    res.send('Welcome to MyFlix!');
+  });
+  
+  app.get('/movies', (req, res) => {
+    res.json(topMovies);
+
+  });
+ 
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
+  
+  // listen for requests
+  app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+  });
+
+ 
