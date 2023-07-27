d9c5f315e7b4df8966047979dc54f537a6894ded
Mark Saville <mark.saville@outlook.com>
2023-07-27 12:00:53 +0200

    add delete user request

--- index.js
+++ index.js
@@ -225,6 +225,20 @@ app.delete('/users/:id/:movieTitle', (req, res) => {
   }
 });
 
+app.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+  let user = users.find(user => user.id == id);
+
+  if (user) {
+    users = users.filter(user => user.id != id);
+    res.status(200).send(`User ${id} deleted`);
+  } else {
+    res.status(400).send('User not found');
+  }
+});
+
+
+
 
 // PUT requests
 
