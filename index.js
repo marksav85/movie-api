78344d2a441d30049ba77db7177b71faf56da1dd
Mark Saville <mark.saville@outlook.com>
2023-08-07 13:14:53 +0200

    update CRUD reqs to interact with mongoose schema

--- index.js
+++ index.js
@@ -15,184 +15,6 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-
-/* let users = [
-  {
-    id: 1,
-    name: 'John Doe',
-    favoriteMovies: ['Fast and Furious 1', 'Fast and Furious 2', 'Fast and Furious 3']
-  },
-  {
-    id: 2,
-    name: 'Jane Doe',
-    favoriteMovies: ['Fast and Furious 4', 'Fast and Furious 5', 'Fast and Furious 6']
-  },
-]
-
-let movies = [
-  {
-    title: 'Fast and Furious 1',
-    description: 'A story about a man named Dom, his car and his ability to defy physics with it',
-    genre: {
-      name: 'Auto-Fantasy',
-      description: 'A genre that uses cars that can perform magic'
-    } ,
-    director: {
-      name: 'Car Fan',
-      bio: 'A director who loves cars',
-      birthYear: '1980',
-      deathYear: '2023'
-    },
-    imageURL: 'insert image url here',
-    featured: true
-  },
-  {
-    title: 'Fast and Furious 2',
-    description: 'A second story about a man named Dom, his car and his ability to defy physics with it',
-    genre: {
-      name: 'Auto-Fantasy',
-      description: 'A genre that uses cars that can perform magic'
-    } ,
-    director: {
-      name: 'Car Fan',
-      bio: 'A director who loves cars',
-      birthYear: '1980',
-      deathYear: '2023'
-    },
-    imageURL: 'insert image url here',
-    featured: true
-  },
-  {
-    title: 'Fast and Furious 3',
-    description: 'A third story about a man named Dom, his car and his ability to defy physics with it',
-    genre: {
-      name: 'Auto-Fantasy',
-      description: 'A genre that uses cars that can perform magic'
-    } ,
-    director: {
-      name: 'Car Fan',
-      bio: 'A director who loves cars',
-      birthYear: '1980',
-      deathYear: '2023'
-    },
-    imageURL: 'insert image url here',
-    featured: true
-  },
-  {
-    title: 'Fast and Furious 4',
-    description: 'A fourth story about a man named Dom, his car and his ability to defy physics with it',
-    genre: {
-      name: 'Auto-Fantasy',
-      description: 'A genre that uses cars that can perform magic'
-    } ,
-    director: {
-      name: 'Car Fan',
-      bio: 'A director who loves cars',
-      birthYear: '1980',
-      deathYear: '2023'
-    },
-    imageURL: 'insert image url here',
-    featured: true
-  },
-  {
-    title: 'Fast and Furious 5',
-    description: 'A fifth story about a man named Dom, his car and his ability to defy physics with it',
-    genre: {
-      name: 'Auto-Fantasy',
-      description: 'A genre that uses cars that can perform magic'
-    } ,
-    director: {
-      name: 'Car Fan',
-      bio: 'A director who loves cars',
-      birthYear: '1980',
-      deathYear: '2023'
-    },
-    imageURL: 'insert image url here',
-    featured: true
-  },
-  {
-    title: 'Fast and Furious 6',
-    description: 'A sixth story about a man named Dom, his car and his ability to defy physics with it',
-    genre: {
-      name: 'Auto-Fantasy',
-      description: 'A genre that uses cars that can perform magic'
-    } ,
-    director: {
-      name: 'Car Fan',
-      bio: 'A director who loves cars',
-      birthYear: '1980',
-      deathYear: '2023'
-    },
-    imageURL: 'insert image url here',
-    featured: true
-  },
-  {
-    title: 'Fast and Furious 7',
-    description: 'A seventh story about a man named Dom, his car and his ability to defy physics with it',
-    genre: {
-      name: 'Auto-Fantasy',
-      description: 'A genre that uses cars that can perform magic'
-    } ,
-    director: {
-      name: 'Car Fan',
-      bio: 'A director who loves cars',
-      birthYear: '1980',
-      deathYear: '2023'
-    },
-    imageURL: 'insert image url here',
-    featured: true
-  },
-  {
-    title: 'Fast and Furious 8',
-    description: 'An eigth story about a man named Dom, his car and his ability to defy physics with it',
-    genre: {
-      name: 'Auto-Fantasy',
-      description: 'A genre that uses cars that can perform magic'
-    } ,
-    director: {
-      name: 'Car Fan',
-      bio: 'A director who loves cars',
-      birthYear: '1980',
-      deathYear: '2023'
-    },
-    imageURL: 'insert image url here',
-    featured: true
-  },
-  {
-    title: 'Fast and Furious 9',
-    description: 'A ninth story about a man named Dom, his car and his ability to defy physics with it',
-    genre: {
-      name: 'Auto-Fantasy',
-      description: 'A genre that uses cars that can perform magic'
-    } ,
-    director: {
-      name: 'Car Fan',
-      bio: 'A director who loves cars',
-      birthYear: '1980',
-      deathYear: '2023'
-    },
-    imageURL: 'insert image url here',
-    featured: true
-  },
-  {
-    title: 'Fast and Furious 10',
-    description: 'A tenth story about a man named Dom, his car and his ability to defy physics with it',
-    genre: {
-      name: 'Auto-Fantasy',
-      description: 'A genre that uses cars that can perform magic'
-    } ,
-    director: {
-      name: 'Car Fan',
-      bio: 'A director who loves cars',
-      birthYear: '1980',
-      deathYear: '2023'
-    },
-    imageURL: 'insert image url here',
-    featured: true
-
-  }
-]; */
-
 // POST requests
 
 //Add a user
@@ -205,7 +27,7 @@ let movies = [
   Birthday: Date
 }*/
 
-/* app.post('/users', aysnc (req, res) => {
+app.post('/users', async (req, res) => {
   await Users.findOne({ Username: req.body.Username })
   .then((user) => {
     if (user) {
@@ -227,7 +49,7 @@ let movies = [
     console.error(error);
     res.status(500).send('Error: ' + error);
   })
-}); */
+});
 
 // Add a movie to a user's list of favorites
 app.post('/users/:Username/movies/:MovieID', async (req, res) => {
@@ -243,19 +65,6 @@ app.post('/users/:Username/movies/:MovieID', async (req, res) => {
     res.status(500).send('Error: ' + error);
   });
 });
-
-
-/* app.post('/users/:id/:movieTitle', (req, res) => {
-  const { id, movieTitle } = req.params;
-  let user = users.find(user => user.id == id);
-
-  if (user) {
-    user.favoriteMovies.push(movieTitle);
-    res.status(200).send(`${movieTitle} added to favorites`);
-  } else {
-    res.status(400).send('User not found');
-  }
-}); */
 
 // DELETE requests
 
@@ -275,31 +84,20 @@ app.delete('/users/:Username', async (req, res) => {
   });
 });
 
-
-/* app.delete('/users/:id/:movieTitle', (req, res) => {
-  const { id, movieTitle } = req.params;
-  let user = users.find(user => user.id == id);
-
-  if (user) {
-    user.favoriteMovies = user.favoriteMovies.filter(title => title !== movieTitle);
-    res.status(200).send(`${movieTitle} removed from favorites`);
-  } else {
-    res.status(400).send('User not found');
-  }
-}); */
-
-/* app.delete('/users/:id', (req, res) => {
-  const { id } = req.params;
-  let user = users.find(user => user.id == id);
-
-  if (user) {
-    users = users.filter(user => user.id != id);
-    res.status(200).send(`User ${id} deleted`);
-  } else {
-    res.status(400).send('User not found');
-  }
-}); */
-
+// Delete a movie from a user's list of favorites
+app.delete('/users/:Username/movies/:MovieID', async (req, res) => {
+  await Users.findOneAndUpdate({ Username: req.params.Username }, {
+    $pull: { FavoriteMovies: req.params.MovieID }
+  },
+  { new: true }) // This line makes sure that the updated document is returned
+  .then((updatedUser) => {
+    res.json(updatedUser);
+  })
+  .catch((error) => {
+    console.error(error);
+    res.status(500).send('Error: ' + error);
+  });
+});
 
 
 
@@ -335,22 +133,9 @@ app.put('/users/:Username', async (req, res) => {
   });
 });
 
-
-/* app.put('/users/:id', (req, res) => {
-  const { id } = req.params;
-  const updatedUser = req.body;
-  let user = users.find(user => user.id == id);
-
-  if (user) {
-    user.name = updatedUser.name;
-    res.status(200).json(user);
-  } else {
-    res.status(400).send('User not found');
-  }
-}); */
-
 // GET requests
 
+// Get all users
 app.get('/users', async (req, res) => {
   await Users.find()
   .then((users) => {
@@ -362,6 +147,7 @@ app.get('/users', async (req, res) => {
   });
 });
 
+// Get a user by username
 app.get('/users/:Username', async (req, res) => {
   await Users.findOne({ Username: req.params.Username })
   .then((user) => {
@@ -373,56 +159,69 @@ app.get('/users/:Username', async (req, res) => {
   });
 });
 
-app.get('/', (req, res) => {
-    res.send('Welcome to MyFlix!');
+// Get all movies
+app.get('/movies', async (req, res) => {
+  await Movies.find()
+  .then((movies) => {
+    res.status(201).json(movies);
+  })
+  .catch((error) => { 
+    console.error(error);
+    res.status(500).send('Error: ' + error);
   });
-  
-/*   app.get('/movies', (req, res) => {
-    res.status(200).json(movies);
-  }); */
+});
 
-  /* app.get('/movies/:title', (req, res) => {
-    const { title } = req.params;
-    const movie = movies.find(movie => movie.title === title);
-   
-    if (movie) {
-      res.status(200).json(movie);
-    } else {
-      res.status(400).send('Movie not found');
-    }
-  }); */
-
-  /* app.get('/movies/genre/:genName', (req, res) => {
-    const { genName } = req.params;
-    const genre = movies.find(movie => movie.genre.name === genName);
-
-    if (genre) {
-      res.status(200).json(genre.genre);
-    } else {
-      res.status(400).send('Genre not found');
-    }
-  }); */
-
- /*  app.get('/movies/director/:dirName', (req, res) => {
-    const { dirName } = req.params;
-    const director = movies.find(movie => movie.director.name === dirName);
-
-    if (director) {
-      res.status(200).json(director.director);
-    } else {
-      res.status(400).send('Director not found');
-    }
-  }); */
-
+// Get a movie by title
+app.get('/movies/:Title', async (req, res) => {
+  await Movies.findOne({ Title: req.params.Title })
+  .then((movie) => {
+    res.json(movie);
+  })
+  .catch((error) => {
+    console.error(error);
+    res.status(500).send('Error: ' + error);
+  });
+});
  
-  app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+// Get movie by genre
+app.get('/movies/genre/:Name', async (req, res) => {
+  await Movies.findOne({ 'Genre.Name': req.params.Name })
+  .then((movie) => {
+    res.json(movie.Genre);
+  })
+  .catch((error) => {
+    console.error(error);
+    res.status(500).send('Error: ' + error);
   });
+});
+
+// Get movie by director
+app.get('/movies/director/:Name', async (req, res) => {
+  await Movies.findOne({ 'Director.Name': req.params.Name })
+  .then((movie) => {
+    res.json(movie.Director);
+  })
+  .catch((error) => {
+    console.error(error);
+    res.status(500).send('Error: ' + error);
+  });
+});
+
+
+// Landing Page welcome text
+app.get('/', (req, res) => {
+  res.send('Welcome to MyFlix!');
+});
+
+// Error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
   
-  // listen for requests
-  app.listen(8080, () => {
-    console.log('Your app is listening on port 8080.');
-  });
+// listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
 
  
